60dea830a002cdc21924aa30cd50fac540b8d055
Jacob Santos <jacob@essential-elements.net>
2015-06-22 00:25:19 -0500

    Add incomplete tests for remaining math functions test suites.

---
+++ tests/ProductFuncTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Unit Test for product Mimic library function.
+ *
+ * @since 0.1.0
+ */
+class ProductFuncTest extends PHPUnit_Framework_TestCase {
+	/*
+	public function testIsClosure_UsingFileExistsFunction() {
+		$callback = \Mimic\bind_callback('file_exists');
+		$this->assertTrue($callback instanceof \Closure);
+	}
+
+	public function testFileExistsFunction_FileDoesNotExist() {
+		$callback = \Mimic\bind_callback('file_exists', __DIR__ . '/nonexistantfile.doesnotexist');
+		$this->assertFalse($callback());
+	}
+
+	public function testFileExistsFunction_DirectoryExists() {
+		$callback = \Mimic\bind_callback('file_exists', __DIR__);
+		$this->assertTrue($callback());
+	}
+
+	public function testFileExistsFunction_FileExists() {
+		$callback = \Mimic\bind_callback('file_exists', __FILE__);
+		$this->assertTrue($callback());
+	}
+	 */
+}
