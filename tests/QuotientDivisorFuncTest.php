5d3032f42dec2801b6c1df6b85c7ca54fd717860
Jacob Santos <jacob@essential-elements.net>
2015-06-22 09:15:43 -0500

    Fix results.

--- tests/QuotientDivisorFuncTest.php
+++ tests/QuotientDivisorFuncTest.php
@@ -10,20 +10,20 @@ use Mimic\Functional as F;
 class QuotientDivisorFuncTest extends PHPUnit_Framework_TestCase {
 	public function dataProvider() {
 		return array(
-			array(null, -1, array(1, 2, 3, 4, 5)),
-			array(null, -1, array(-1, -2, -3, -4, -5)),
-			array(null, -1, array(-1, -2, -3, -4, -5, 0)),
-			array(null, -1, array(1.5, 2.5, 3.5, 4.5, 5.5)),
-			array(null, -1, array(-1.5, -2.5, -3.5, -4.5, -5.5)),
-			array(null, -1, array(-1.5, -2.5, -3.5, -4.5, -5.5, 0.0)),
+			array(null, 1.875, array(1, 2, 3, 4, 5)),
+			array(null, -1.875, array(-1, -2, -3, -4, -5)),
+			array(null, 2.5666666666666669, array(-1, -2, -3, -4, -5, 0)),
+			array(null, -2.5666666666666669, array(1.5, 2.5, 3.5, 4.5, 5.5)),
+			array(null, 0.0, array(100, 10, 5)),
+			array(null, 0.0, array(100, 10, 5, 0.0)),
 			array(null, 1,  array('', 'something5', 'else', 'what')),
-			array(5, -1, array(1, 2, 3, 4, 5)),
-			array(5, -1, array(-1, -2, -3, -4, -5)),
-			array(5, -1, array(-1, -2, -3, -4, -5, 0)),
-			array(5, -1, array(1.5, 2.5, 3.5, 4.5, 5.5)),
-			array(5, -1, array(-1.5, -2.5, -3.5, -4.5, -5.5)),
-			array(5, -1, array(-1.5, -2.5, -3.5, -4.5, -5.5, 0.0)),
-			array(5, 1,  array('', 'something5', 'else', 'what')),
+			array(5, 0.375, array(1, 2, 3, 4, 5)),
+			array(5, -0.375, array(-1, -2, -3, -4, -5)),
+			array(5, 0, array(-100, -10, -5, 0)),
+			array(5, 0.51333333333333331, array(1.5, 2.5, 3.5, 4.5, 5.5)),
+			array(5, -0.51333333333333331, array(-1.5, -2.5, -3.5, -4.5, -5.5)),
+			array(5, 0.0, array(-1.5, -2.5, -3.5, -4.5, -5.5, 0.0)),
+			array(5, 5,  array('', 'something5', 'else', 'what')),
 		);
 	}
 
