1d0fb6a1b0dfca6c12a697dff897a62f638ebcd1
Jacob Santos <jacob@essential-elements.net>
2015-06-21 23:58:00 -0500

    Add initial value test cases to cover that use case.

--- tests/DifferenceFuncTest.php
+++ tests/DifferenceFuncTest.php
@@ -29,4 +29,24 @@ class DifferenceFuncTest extends PHPUnit_Framework_TestCase {
 	public function testDifferenceWithStringIsZero() {
 		$this->assertEquals(0, F\difference(array('', 'something5', 'else', 'what')));
 	}
+
+	public function testDifferenceIsNegative15_initialValue() {
+		$this->assertEquals(-20, F\difference(array(1, 2, 3, 4, 5), 5));
+	}
+
+	public function testDifferenceIs15_initialValue() {
+		$this->assertEquals(20, F\difference(array(-1, -2, -3, -4, -5), -5));
+	}
+
+	public function testDifferenceWithFloatIsNegativeFloat_initialValue() {
+		$this->assertEquals(-22.5, F\difference(array(1.5, 2.5, 3.5, 4.5, 5.5), 5));
+	}
+
+	public function testDifferenceWithFloatIsFloat_initialValue() {
+		$this->assertEquals(23, F\difference(array(-1.5, -2.5, -3.5, -4.5, -5.5), -5.5));
+	}
+
+	public function testDifferenceWithStringIsZero_initialValue() {
+		$this->assertEquals(5, F\difference(array('', 'something5', 'else', 'what'), 5));
+	}
 }
