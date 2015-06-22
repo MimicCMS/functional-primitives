b5d306cca4fe0d78302020c285ce7643b1e8342e
Jacob Santos <jacob@essential-elements.net>
2015-06-22 08:16:48 -0500

    Use data provider to complete collection number operation test suite.

---
+++ tests/CollectionNumberOperationFuncTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Mimic\Functional as F;
+
+/**
+ * Unit Test for collectionNumberOperation Mimic library function.
+ *
+ * @since 0.1.0
+ */
+class CollectionNumberOperationFuncTest extends PHPUnit_Framework_TestCase {
+
+	public function dataProvider() {
+		return array(
+			array(1, 0, 1, true),
+			array(1.25, 0, 1.25, true),
+			array('1', 0, '1', true),
+			array('1.25', 0, '1.25', true),
+			array('something', 1, 1, false),
+		);
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 */
+	public function testDataProviderResults($element, $current, $expected, $willBeCalled) {
+		$called = false;
+		$callback = function($element, $current) use (&$called) {
+			$called = true;
+		};
+		$operation = F\collectionNumberOperation($callback);
+		$actual = $operation($element, $current);
+		$this->assertEquals($willBeCalled, $called);
+		$this->assertEquals($expected, $actual);
+	}
+}
