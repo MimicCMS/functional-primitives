73ff9178a5a78a3324070d3de85e82e0868be6a1
Jacob Santos <jacob@essential-elements.net>
2015-06-22 00:01:30 -0500

    Remove maximum from name.

--- tests/MaximumFuncTest.php
+++ tests/MaximumFuncTest.php
@@ -8,19 +8,19 @@ use Mimic\Functional as F;
  * @since 0.1.0
  */
 class MaximumFuncTest extends PHPUnit_Framework_TestCase {
-	public function testMaximumIsFour() {
+	public function testIsFour() {
 		$this->assertEquals(4, F\maximum(array(0, 1, 2, 3, 4)));
 	}
 
-	public function testMaximumIsTwoWithStrings() {
+	public function testIsTwoWithStrings() {
 		$this->assertEquals(2, F\maximum(array(0, '', 'something', 'what', 1, 2)));
 	}
 
-	public function testMaximumWithFloatsGetsCorrect() {
+	public function testWithFloatsGetsCorrect() {
 		$this->assertEquals(3.56, F\maximum(array(1.5, 2.5, 2.5, 2.5, 3.5, 3.56)));
 	}
 
-	public function testMaximumWithoutNumericIsNull() {
+	public function testWithoutNumericIsNull() {
 		$this->assertEquals(null, F\maximum(array('', 'Something5', 'okay', 'what')));
 	}
 }
