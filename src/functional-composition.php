f0d59cfb3990d509855b5a166b59ca57d4cce8ed
Jacob Santos <jacob@essential-elements.net>
2015-06-21 21:07:49 -0500

    Add new file for composing functions. Might add curry functions to this as well.

---
+++ src/functional-composition.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package Mimic\Functional
+ * @since 0.1.0
+ * @license MIT
+ *
+ * @typedef MapCollectionCallback \Closure {
+ *     @param mixed $element
+ *       Item in the collection.
+ *     @param string|int $index
+ *       Index of element in the collection.
+ *     @param \Traversable|array $collection
+ *       Collection. Writes to collection will not do anything.
+ *     @return mixed
+ * }
+ *
+ * @typedef ReduceCollectionCallback \Closure {
+ *     @param mixed $element
+ *       Item in the collection.
+ *     @param mixed $current
+ *       Current value after modification.
+ *     @param string|int $index
+ *       Index of element in the collection.
+ *     @param \Traversable|array $collection
+ *       Collection. Writes to collection will not do anything.
+ *     @return mixed
+ * }
+ */
+
+/** @package Mimic\Functional */
+namespace Mimic\Functional;
+
+
+function compose() {
+	/** @todo Incomplete */
+}
+
+function pipeline() {
+
+}
+
