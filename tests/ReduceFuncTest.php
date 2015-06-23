201587a9e02ca00fef4f0c2c0675f40e3dce05f6
Jacob Santos <jacob@essential-elements.net>
2015-06-23 17:24:17 -0500

    Add initial incomplete reduce.

---
+++ tests/ReduceFuncTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Mimic\Functional as F;
+
+/**
+ * Unit Test for reduce Mimic library function.
+ *
+ * @since 0.1.0
+ */
+class ReduceFuncTest extends PHPUnit_Framework_TestCase {
+	public function dataProvider() {
+		return array(
+			array(2.5, array(1, 2, 3, 4)),
+			array(2, array(0, '', 'something', 'what', 1, 2, 3, 4)),
+			array(2.75, array(1.5, 2.5, 2.5, 2.5, 3.5, 4)),
+			array(2.5, array(2.5, 2.5, 2.5, 2.5, 2.5, 2.5)),
+			array(2, array(2, 2, 2, 2, 2, 2)),
+			array(0, array('', 'Something5', 'okay', 'what')),
+		);
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @covers ::Mimic\Functional\reduce
+	 */
+	public function testResults($result, $collection) {
+		$this->assertEquals($result, F\reduce($collection));
+	}
+}
