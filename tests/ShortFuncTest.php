17f7153141225dcb6936b3ac80b07bf6f993e1f2
Jacob Santos <jacob@essential-elements.net>
2015-06-23 18:42:02 -0500

    Fix test.

--- tests/ShortFuncTest.php
+++ tests/ShortFuncTest.php
@@ -14,14 +14,14 @@ class ShortFuncTest extends PHPUnit_Framework_TestCase {
 		$values = array_fill(0, 5, 1);
 		$keys = array_fill_keys(array('index1', 'index2', 'index3', 'index4', 'index5'), 1);
 		return array(
-			array(false, true, true,  5, $values),
+			array(false, true, true,  6, $values),
 			array(false, true, false, 3, $values),
-			array(false, true, true,  5, $keys),
+			array(false, true, true,  6, $keys),
 			array(false, true, false, 3, $keys),
 			array(true, false, true,  3, $values),
-			array(true, false, false, 5, $values),
+			array(true, false, false, 6, $values),
 			array(true, false, true,  3, $keys),
-			array(true, false, false, 5, $keys),
+			array(true, false, false, 6, $keys),
 		);
 	}
 
@@ -43,6 +43,8 @@ class ShortFuncTest extends PHPUnit_Framework_TestCase {
 		};
 
 		$this->assertEquals($expected, F\short($data, $passed, $default, $callback));
-		$this->assertEquals($stop, $executed, 'stop does not equal executed');
+
+		$expected = $stop > count($data) ? count($data) : $stop;
+		$this->assertEquals($expected, $executed, 'stop does not equal executed');
 	}
 }
