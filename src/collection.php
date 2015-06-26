b47639a9e367156572b1604c3f2e290758d0e003
Jacob Santos <jacob@essential-elements.net>
2015-06-26 17:58:46 -0500

    * Update documentation. * Complete indexesOf function. * Complete reduceLeft function. * Complete reduceRight function. * Complete unique function. * Complete divide function. * Complete set function. * Complete only function. * Add skeletons for other Laravel helpers.

--- src/collection.php
+++ src/collection.php
@@ -152,6 +152,7 @@ function none($collection, $callback) {
  *
  * @api
  * @since 0.1.0
+ * @todo Needs tests.
  *
  * @param Traversable|array $collection
  * @param MapCollectionCallback|callable $callback
@@ -176,6 +177,7 @@ function dropFirst($collection, $callback) {
  *
  * @api
  * @since 0.1.0
+ * @todo Needs tests.
  *
  * @param Traversable|array $collection
  * @param MapCollectionCallback|callable $callback
@@ -249,6 +251,10 @@ function first($collection, $callback = null) {
  * First index in collection matching value, using value as callback if
  * available.
  *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ *
  * @param Traversable|array $collection
  * @param MapCollectionCallback|callable|mixed $value
  *   A mixed value allows checking for the given value against element returning
@@ -270,6 +276,30 @@ function firstIndexOf($collection, $value, $strict = true) {
 	return firstIndexOf($collection, compare($value, $strict));
 }
 
+/**
+ * List of all indexes that match elements in a collection.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ * @todo Need to add to guide documentation.
+ *
+ * @param Traversable|array $collection
+ * @param MapCollectionCallback|callable|mixed $value
+ *   A mixed value allows checking for the given value against element returning
+ *   index in collection.
+ * @param boolean $strict
+ *   Optional. Defaults to true. Only used when value is mixed or not callable.
+ * @return boolean|string|numeric
+ *   False on failure.
+ */
+function indexesOf($collection, $value, $strict = true) {
+	if ( is_callable($value) ) {
+		return array_keys(filter($collection, $value));
+	}
+	return indexesOf($collection, compare($value, $strict));
+}
+
 function flatMap() {
 	/** @todo Incomplete */
 }
@@ -280,6 +310,10 @@ function flatten() {
 
 /**
  * Groups a collection by callback.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
  *
  * @param Traversable|array $collection
  * @param MapCollectionCallback|callable $callback
@@ -425,6 +459,10 @@ function last($collection, $callback = null) {
  * Last index in collection matching value, using value as callback if
  * available.
  *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ *
  * @param Traversable|array $collection
  * @param MapCollectionCallback|callable|mixed $value
  *   A mixed value allows checking for the given value against element returning
@@ -440,6 +478,10 @@ function lastIndexOf($collection, $value, $strict = true) {
 
 /**
  * Separates a collection by callback into truthy and falsy parts.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
  *
  * @param Traversable|array $collection
  * @param MapCollectionCallback|callable $callback
@@ -462,12 +504,38 @@ function pluck() {
 	/** @todo Incomplete */
 }
 
-function reduceLeft() {
-	/** @todo Incomplete */
+/**
+ * Iterate through each element of collection to reduce to a single value.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs Tests.
+ *
+ * @param Traversable|array $collection
+ * @param ReduceCollectionCallback|callable $callback
+ * @param mixed $initial
+ *   Optional. Default to null.
+ * @return mixed
+ */
+function reduceLeft($collection, $callback, $initial = null) {
+	return reduce($collection, $initial, $callback);
 }
 
-function reduceRight() {
-	/** @todo Incomplete */
+/**
+ * Iterate through each element of collection to reduce to a single value.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs Tests.
+ *
+ * @param Traversable|array $collection
+ * @param ReduceCollectionCallback|callable $callback
+ * @param mixed $initial
+ *   Optional. Default to null.
+ * @return mixed
+ */
+function reduceRight($collection, $callback, $initial = null) {
+	return reduce(array_reverse($collection, true), $initial, $callback);
 }
 
 /**
@@ -507,6 +575,10 @@ function select($collection, $callback) {
  * This will short-circuit the loop, returning true immediately on the first
  * element where the callback is true.
  *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ *
  * @param Traversable|array $collection
  * @param MapCollectionCallback|callable $callback
  * @return bool
@@ -518,9 +590,14 @@ function some($collection, $callback) {
 /**
  * Sorts a collection with a user-defined function, optionally perserving keys.
  *
+ * @api
+ * @since 0.1.0
+ *
  * @link https://github.com/lstrojny/functional-php See src/Functional/Sort.php
  * @license MIT
  * @copyright 2011-2015 Lars Strojny <lstrojny@php.net>
+ *
+ * @todo Needs tests.
  *
  * @param Traversable|array $collection
  * @param callable $callback {
@@ -566,10 +643,189 @@ function tail($collection, $callback = null) {
 	return last($collection, $callback);
 }
 
-function unique() {
-	/** @todo Incomplete */
+/**
+ * Pull unique values from a collection using an optional callback.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ *
+ * @param Traversable|array $collection
+ * @param boolean $strict
+ *   Optional. Default is true. Whether check should use strict comparison.
+ * @param MapCollectionCallback|callable|null $callback
+ *   Optional. If given, element will be passed to callback for the key to check
+ *   against whether it is unique.
+ * @return array
+ */
+function unique($collection, $strict = true, $callback = null) {
+	$existing = array();
+	return filter($collection, function($element, $index, $collection) use ($callback, $strict, &$existing) {
+		if ( is_callable($callback) ) {
+			$key = $callback($element, $index, $collection);
+		}
+		else {
+			$key = $element;
+		}
+
+		if ( ! in_array($key, $existing, $strict) ) {
+			$existing[] = $key;
+			return true;
+		}
+		return false;
+	});
 }
 
 function zip() {
 	/** @todo Incomplete */
+}
+
+/**
+ * Split the collection into separate arrays for indexes and values.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ * @link http://laravel.com/docs/master/helpers#method-array-divide
+ *
+ * @param Traversable|array $collection
+ * @return array
+ *   [0] are the collection indexes and [1] are the collection values.
+ */
+function divide($collection) {
+	if ($collection instanceof Traversable) {
+		$collection = iterator_to_array($collection);
+	}
+	return array(
+		array_keys($collection),
+		array_values($collection)
+	);
+}
+
+/**
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ * @todo Complete
+ * @link http://laravel.com/docs/master/helpers#method-array-dot
+ */
+function flattenDot() {
+	/** @todo Incomplete */
+}
+
+/**
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ * @todo Complete
+ * @link http://laravel.com/docs/master/helpers#method-array-except
+ */
+function except() {
+	/** @todo Incomplete */
+}
+
+/**
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ * @todo Complete
+ * @link http://laravel.com/docs/master/helpers#method-array-forget
+ */
+function forget() {
+	/** @todo Incomplete */
+}
+
+/**
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ * @todo Complete
+ * @link http://laravel.com/docs/master/helpers#method-array-get
+ */
+function get() {
+	/** @todo Incomplete */
+}
+
+/**
+ * Mutate collection using dot notation to lookup indexes.
+ *
+ * This can modify both objects and arrays. It can also update combination of
+ * objects and arrays. So it will attempt to either modify the property or index
+ * value.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ * @link http://laravel.com/docs/master/helpers#method-array-set
+ *
+ * @param Traversable|array $collection
+ * @param string $lookup
+ * @param mixed $value
+ * @return Traversable|array
+ *   Copy of collection with modification applied.
+ */
+function set($collection, $lookup, $value) {
+	$indexes = array($lookup);
+	if (strpos('.', $lookup) !== false) {
+		$indexes = explode('.');
+	}
+
+	$store = array($collection);
+	$current = $collection;
+	foreach ( array_slice($indexes, 0, -1) as $index ) {
+		if ( is_object($current) && property_exists($current, $index) ) {
+			$store[] = $current->{$index};
+			$current = $current->{$index};
+		}
+		else if ( is_array($current) && array_key_exists($index, $current) ) {
+			$store[] = $current[$index];
+			$current = $current[$index];
+		}
+	}
+
+	$index = array_pop($indexes);
+	if ( is_object($current) && property_exists($current, $index) ) {
+		$current->{$index} = $value;
+	}
+	else if ( is_array($current) && array_key_exists($index, $current) ) {
+		$current[$index] = $value;
+	}
+
+	$build = $current;
+	foreach (array_reverse($indexes) as $index) {
+		$temp = array_pop($store);
+		if ( is_object($temp) && property_exists($temp, $index) ) {
+			$temp->{$index} = $build;
+		}
+		else if ( is_array($temp) && array_key_exists($index, $temp) ) {
+			$temp[$index] = $build;
+		}
+		$build = $temp;
+	}
+
+	return $build;
+}
+
+/**
+ * Keep only the given elements that match given indexes.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ * @link http://laravel.com/docs/master/helpers#method-array-only
+ *
+ * @param Traversable|array $collection
+ * @param array $keep
+ *   List of indexes.
+ * @return array
+ *   Array of elements in collection that are in keys list.
+ */
+function only($collection, array $keep) {
+	return filter($collection, function($_, $index) use ($keep) {
+		return in_array($index, $keep);
+	});
 }
