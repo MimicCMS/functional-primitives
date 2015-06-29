907d4fa99bec67c2110b4ea8c0294b2457270385
Jacob Santos <jacob@essential-elements.net>
2015-06-28 19:14:26 -0500

    Order helpers and add additional helper functions.

--- src/helpers.php
+++ src/helpers.php
@@ -22,6 +22,45 @@ use Closure;
 use Traversable;
 
 /**
+ * Execute callback when test callback passed.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ *
+ * @param callable $callback
+ * @param callable $test
+ * @return Closure {
+ *     @return boolean|mixed
+ *       False on failure.
+ * }
+ */
+function attempt($callback, $test) {
+	return function() use ($test, $callback) {
+		if ($test() === false) {
+			return false;
+		}
+		return $callback();
+	};
+}
+
+/**
+ * Comparison function which checks whether the element matches given value.
+ *
+ * @api
+ * @since 0.1.0
+ *
+ * @param mixed $value
+ * @param boolean $strict
+ * @return MapCollectionCallback|callable
+ */
+function compare($value, $strict = false) {
+	return function($element) use ($value, $strict) {
+		return $value === $element || (!$strict && $value == $element);
+	};
+}
+
+/**
  * Whether value exists in collection.
  *
  * @api
@@ -34,6 +73,48 @@ use Traversable;
  */
 function contains($collection, $value, $strict = false) {
 	return short($collection, true, false, compare($value, $strict));
+}
+
+/**
+ * Executes left callback and if it fails will execute the right callback.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ *
+ * @param callable $left
+ * @param callable $right
+ * @return Closure {
+ *     @return mixed
+ * }
+ */
+function either($left, $right) {
+	return function() use ($left, $right) {
+		$value = $left();
+		if ($value !== false) {
+			return $value;
+		}
+		return $right();
+	};
+}
+
+/**
+ * Execute callback.
+ *
+ * @param callable ...$args
+ * @return array<mixed>
+ */
+function execute() {
+	if (func_num_args() < 1) {
+		return;
+	}
+	$values = array();
+	foreach (func_get_args() as $callback) {
+		if ( is_callable($callback) ) {
+			$values = $callback();
+		}
+	}
+	return $values;
 }
 
 /**
@@ -60,6 +141,112 @@ function false($collection) {
  */
 function falsy($collection) {
 	return every($collection, compare(false, false));
+}
+
+/**
+ * Hashes an array preventing closures from throwing errors.
+ *
+ * This is not really part of the API and you should probably use something else
+ * if you want this implemented correctly. It is not tuned for performance.
+ *
+ * @api
+ * @since 0.1.0
+ *
+ * @todo Needs tests.
+ *
+ * @param array<mixed> $args
+ * @return string
+ */
+function hash_array(array $args = array()) {
+	$serializableArgs = array();
+	foreach ( $args as $arg ) {
+		if (is_object($arg) || $arg instanceof Closure) {
+			$serializableArgs[] = get_class($arg) .':'. spl_object_hash($arg);
+		}
+		else if (is_array($arg)) {
+			$serializableArgs[] = hash_array($arg);
+		}
+		else {
+			$serializableArgs[] = $arg;
+		}
+	}
+
+	return json_encode($serializableArgs);
+}
+
+/**
+ * Memorize caches the result for a given set of arguments.
+ *
+ * Functions or callbacks that use this must have pure functions or callbacks
+ * that return the same result every time the same set of arguments are passed.
+ * Not following this will break the execution of the process and given
+ * inaccurate results.
+ *
+ * @api
+ * @since 0.1.0
+ *
+ * @todo Needs tests.
+ *
+ * @param callable $callback
+ * @return MemoizeCache
+ */
+function memoize($callback) {
+	return new MemoizeCache($callback);
+}
+
+/**
+ * Negate result of collection item callback.
+ *
+ * @api
+ * @since 0.1.0
+ *
+ * @param MapCollectionCallback|callable
+ * @return MapCollectionCallback|callable
+ */
+function negate($callback) {
+	return function($element, $index, $collection) use ($callback) {
+		return ! $callback($element, $index, $collection);
+	};
+}
+
+/**
+ * Put value onto object or array by name.
+ *
+ * This works with both objects and arrays. Other types will simply be returned
+ * with no modifications.
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests.
+ * @link http://laravel.com/docs/master/helpers#method-array-add
+ *
+ * @param object|array $something
+ * @param string $name
+ * @param mixed $value
+ * @param boolean $override
+ *   Optional. Default is false. Will allow for ignoring, if current value
+ *   exists or overriding current value.
+ * @return object|array
+ *   Copy of something with new value.
+ */
+function put($something, $name, $value, $override = false) {
+	if ( is_object($something) && property_exists($something, $name) ) {
+		$copy = clone $something;
+		if ( $copy->{$name} === null || $override ) {
+			$copy->{$name} = $value;
+		}
+		return $copy;
+	}
+
+	if ( is_array($something) && ( !array_key_exists($name, $something) || $override ) ) {
+		$something[$name] = $value;
+	}
+
+	return $something;
+}
+
+function retry() {
+	/** @todo Incomplete */
 }
 
 /**
@@ -129,127 +316,3 @@ function value($value) {
 function with($value, $callback) {
 	return $callback(value($value));
 }
-
-/**
- * Memorize caches the result for a given set of arguments.
- *
- * Functions or callbacks that use this must have pure functions or callbacks
- * that return the same result every time the same set of arguments are passed.
- * Not following this will break the execution of the process and given
- * inaccurate results.
- *
- * @api
- * @since 0.1.0
- *
- * @todo Needs tests.
- *
- * @param callback $callback
- * @return MemoizeCache
- */
-function memoize($callback) {
-	return new MemoizeCache($callback);
-}
-
-/**
- * Negate result of collection item callback.
- *
- * @api
- * @since 0.1.0
- *
- * @param MapCollectionCallback|callable
- * @return MapCollectionCallback|callable
- */
-function negate($callback) {
-	return function($element, $index, $collection) use ($callback) {
-		return ! $callback($element, $index, $collection);
-	};
-}
-
-function retry() {
-	/** @todo Incomplete */
-}
-
-/**
- * Put value onto object or array by name.
- *
- * This works with both objects and arrays. Other types will simply be returned
- * with no modifications.
- *
- * @api
- * @since 0.1.0
- * @todo Needs tests.
- * @link http://laravel.com/docs/master/helpers#method-array-add
- *
- * @param object|array $something
- * @param string $name
- * @param mixed $value
- * @param boolean $override
- *   Optional. Default is false. Will allow for ignoring, if current value
- *   exists or overriding current value.
- * @return object|array
- *   Copy of something with new value.
- */
-function put($something, $name, $value, $override = false) {
-	if ( is_object($something) && property_exists($something, $name) ) {
-		$copy = clone $something;
-		if ( $copy->{$name} === null || $override ) {
-			$copy->{$name} = $value;
-		}
-		return $copy;
-	}
-
-	if ( is_array($something) && ( !array_key_exists($name, $something) || $override ) ) {
-		$something[$name] = $value;
-	}
-
-	return $something;
-}
-
-/**
- * Comparison function which checks whether the element matches given value.
- *
- * @api
- * @since 0.1.0
- *
- * @param mixed $value
- * @param boolean $strict
- * @return MapCollectionCallback|callable
- */
-function compare($value, $strict = false) {
-	return function($element) use ($value, $strict) {
-		return $value === $element || (!$strict && $value == $element);
-	};
-}
-
-/**
- * Hashes an array preventing closures from throwing errors.
- *
- * This is not really part of the API and you should probably use something else
- * if you want this implemented correctly. It is not tuned for performance.
- *
- * @api
- * @since 0.1.0
- *
- * @todo Needs tests.
- *
- * @param array<mixed> $args
- * @return string
- */
-function hash_array(array $args = array()) {
-	$serializableArgs = array();
-	foreach ( $args as $arg ) {
-		if (is_object($arg) || $arg instanceof Closure) {
-			$serializableArgs[] = get_class($arg) .':'. spl_object_hash($arg);
-		}
-		else if (is_array($arg)) {
-			$serializableArgs[] = hash_array($arg);
-		}
-		else {
-			$serializableArgs[] = $arg;
-		}
-	}
-
-	return json_encode($serializableArgs);
-}
-
-
