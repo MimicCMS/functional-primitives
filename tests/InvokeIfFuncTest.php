241feb2383b556b7f151c1615c156ff54920a67d
Jacob Santos <jacob@essential-elements.net>
2015-07-09 18:35:26 -0500

    Add invoke if function test suite.

---
+++ tests/InvokeIfFuncTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Mimic\Test;
+
+use PHPUnit_Framework_TestCase;
+use Mimic\Functional as F;
+
+/**
+ * Unit Test for dropFirst Mimic library function.
+ *
+ * @since 0.1.0
+ */
+class InvokeIfFuncTest extends PHPUnit_Framework_TestCase {
+	public function dataProvider() {
+		return array(
+			array(true, array(), new Stub\InvokeTrue, 'exists', null),
+			array(false, array(), new Stub\InvokeFalse, 'exists', null),
+			array(null, array(), new Stub\InvokeTrue, '__doesnotexist__', null),
+			array(false, array(), new Stub\InvokeTrue, '__doesnotexist__', false),
+			array(true, array(), new Stub\InvokeTrue, '__doesnotexist__', true),
+			array('something', array(), new Stub\InvokeTrue, '__doesnotexist__', 'something'),
+			array(array(1, 2, 3), array(1, 2, 3), '\Mimic\Test\Stub\InvokeTrue', 'with', null),
+			array(array(3, 2, 1), array(1, 2, 3), '\Mimic\Test\Stub\InvokeFalse', 'with', null),
+		);
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @covers ::Mimic\Functional\invokeIf
+	 */
+	public function testInstanceMethod_InvokeTrue($expected, $args, $obj, $methodName, $default) {
+		$this->assertEquals($expected, F\invokeIf($obj, $methodName, $args, $default));
+	}
+}
