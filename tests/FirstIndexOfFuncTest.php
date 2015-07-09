561b9743c1b205cfa6b8d9e7c05709fd50519d4a
Jacob Santos <jacob@essential-elements.net>
2015-07-09 11:23:59 -0500

    Update boolean to add more values for testing accuracy.

--- tests/FirstIndexOfFuncTest.php
+++ tests/FirstIndexOfFuncTest.php
@@ -12,7 +12,7 @@ use Mimic\Functional as F;
  */
 class FirstIndexOfFuncTest extends PHPUnit_Framework_TestCase {
 	public function dataProvider() {
-		$boolean = array(0, 1, false, true);
+		$boolean = array(0, 1, false, false, true, true, 0, 1);
 		$integer = array('1', 1);
 		$float = array('1.2', 1, 1.2);
 		$collection = array(0, 1, false, true, 1.0, 'something', 'else', '1');
