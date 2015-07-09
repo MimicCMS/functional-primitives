ea409e7fce0f334da16df328300d0717a0fa7cae
Jacob Santos <jacob@essential-elements.net>
2015-07-09 17:46:31 -0500

    Add either function test suite.

---
+++ tests/EitherFuncTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Mimic\Test;
+
+use PHPUnit_Framework_TestCase;
+use Mimic\Functional as F;
+
+/**
+ * Unit Test for either Mimic library function.
+ *
+ * @since 0.1.0
+ */
+class EitherFuncTest extends PHPUnit_Framework_TestCase {
+	public function dataProvider() {
+		return array(
+			array('something'),
+			array(0),
+			array(1),
+			array(true),
+			array(1.0),
+		);
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @covers ::Mimic\Functional\either
+	 */
+	public function testFirstCallbackValue($expected) {
+		$left = function() use ($expected) { return $expected; };
+		$right = function() { return false; };
+		$this->assertEquals($expected, F\either($left, $right));
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @covers ::Mimic\Functional\either
+	 */
+	public function testSecondCallbackValue($expected) {
+		$left = function() { return false; };
+		$right = function() use ($expected) { return $expected; };
+		$this->assertEquals($expected, F\either($left, $right));
+	}
+}
