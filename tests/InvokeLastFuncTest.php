36119c4684540f050c25b4e4833565dc5a7c28ae
Jacob Santos <jacob@essential-elements.net>
2015-07-09 18:35:43 -0500

    Add invoke last function test suite.

---
+++ tests/InvokeLastFuncTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mimic\Test;
+
+use PHPUnit_Framework_TestCase;
+use Mimic\Functional as F;
+
+/**
+ * Unit Test for invokeLast Mimic library function.
+ *
+ * @since 0.1.0
+ */
+class InvokeLastFuncTest extends PHPUnit_Framework_TestCase {
+	public function dataProvider() {
+		return array(
+			array(false, array(), 'exists'),
+			array(null, array(), '__doesnotexist__'),
+			array(array(3, 2, 1), array(1, 2, 3), 'with'),
+		);
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @covers ::Mimic\Functional\invokeLast
+	 */
+	public function testInstanceMethods($expected, $args, $methodName) {
+		$collection = array(new Stub\InvokeTrue, new Stub\InvokeFalse);
+		$this->assertEquals($expected, F\invokeLast($collection, $methodName, $args));
+	}
+
+	/**
+	 * @covers ::Mimic\Functional\invokeLast
+	 */
+	public function testClassMethod() {
+		$collection = array('\Mimic\Test\Stub\InvokeTrue', '\Mimic\Test\Stub\InvokeFalse');
+		$this->assertFalse(F\invokeLast($collection, 'method'));
+	}
+}
