57afe2d749c84ec8bfe9528bb6c40f3c035eb112
Jacob Santos <jacob@essential-elements.net>
2015-07-10 12:06:26 -0500

    Add divide function test suite. This is incomplete and missing additional code path.

---
+++ tests/DivideFuncTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mimic\Test;
+
+use PHPUnit_Framework_TestCase;
+use Mimic\Functional as F;
+
+/**
+ * Unit Test for true Mimic library function.
+ *
+ * @since 0.1.0
+ *
+ * @todo Need to use QuickTest library.
+ */
+class DivideFuncTest extends PHPUnit_Framework_TestCase {
+	public function dataProvider() {
+		return array(
+			array(
+				array(1, 2, 3),
+				array(array(0, 1, 2), array(1, 2, 3))
+			),
+			array(
+				array('thing1' => 1, 'thing2' => 2, 'thing3' => 3),
+				array(array('thing1', 'thing2', 'thing3'), array(1, 2, 3))
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @covers ::Mimic\Functional\compare
+	 */
+	public function testResults($collection, $expected) {
+		$actual = F\divide($collection);
+		$this->assertEquals($expected, $actual);
+	}
+}
