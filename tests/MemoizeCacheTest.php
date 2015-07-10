76eac325429fbfdd9a6d856b281e46af64d80b81
Jacob Santos <jacob@essential-elements.net>
2015-07-09 23:02:55 -0500

    Add Memoize Cache class test suite.

---
+++ tests/MemoizeCacheTest.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Mimic\Test;
+
+use PHPUnit_Framework_TestCase;
+use Mimic\Functional\MemoizeCache;
+
+/**
+ * Unit Test for MemoizeCache Mimic library class.
+ *
+ * @since 0.1.0
+ */
+class MemoizeCacheFuncTest extends PHPUnit_Framework_TestCase {
+
+	private $memoize;
+
+	private $executed = array();
+
+	public function setUp() {
+		$self = $this;
+		$test = function() use ($self) {
+			$args = func_get_args();
+			$hash = \json_encode($args);
+			if (! isset($self->executed[ $hash ]) ) {
+				$self->executed[ $hash ] = 0;
+			}
+			$self->executed[ $hash ] += 1;
+			return $args;
+		};
+
+		$this->memoize = new MemoizeCache($test);
+
+	}
+	/**
+	 * @covers \Mimic\Functional\MemoizeCache::__construct
+	 */
+	public function testCallbackPropertyMatchesOriginal() {
+		$expected = function() { return true; };
+		$memoize = new MemoizeCache($expected);
+		$reflect = new \ReflectionClass($memoize);
+		$actual = $reflect->getProperty('_callback')->getValue();
+		$this->assertSame($expected, $actual);
+	}
+
+	/**
+	 * @covers \Mimic\Functional\MemoizeCache::__invoke
+	 */
+	public function testCallbackArguments() {
+		$memoize = $this->memoize;
+		$this->assertEquals(array(), $memoize());
+		$this->assertEquals(1, $this->executed[0]);
+
+		$this->assertEquals(array(), $memoize());
+		$this->assertEquals(1, $this->executed[0]);
+
+		$this->assertEquals(array(1), $memoize(1));
+		$this->assertEquals(1, $this->executed[1]);
+
+		$this->assertEquals(array(1), $memoize(1));
+		$this->assertEquals(1, $this->executed[1]);
+
+		$this->assertEquals(array(), $memoize());
+		$this->assertEquals(1, $this->executed[0]);
+
+		$this->assertEquals(array(1, 2), $memoize(1, 2));
+		$this->assertEquals(1, $this->executed[2]);
+
+		$this->assertEquals(array(1, 2), $memoize(1, 2));
+		$this->assertEquals(1, $this->executed[2]);
+	}
+
+	/**
+	 * @covers \Mimic\Functional\MemoizeCache::__invoke
+	 */
+	public function testCallParametersOnlyExecutesOnce_SameArgNum() {
+		$memoize = $this->memoize;
+
+		$expected = array();
+		$this->assertEquals($expected, $memoize());
+		$this->assertEquals(1, $this->executed[\json_encode($expected)]);
+
+		$this->assertEquals($expected, $memoize());
+		$this->assertEquals(1, $this->executed[\json_encode($expected)]);
+
+		$expected = array(1);
+		$this->assertEquals($expected, $memoize(1));
+		$this->assertEquals(1, $this->executed[\json_encode($expected)]);
+
+		$this->assertEquals($expected, $memoize(1));
+		$this->assertEquals(1, $this->executed[\json_encode($expected)]);
+
+		$expected = array(2);
+		$this->assertEquals($expected, $memoize(2));
+		$this->assertEquals(1, $this->executed[\json_encode($expected)]);
+
+		$this->assertEquals($expected, $memoize(2));
+		$this->assertEquals(1, $this->executed[\json_encode($expected)]);
+	}
+
+	/**
+	 * @covers \Mimic\Functional\MemoizeCache::clean
+	 */
+	public function testClean1Args() {
+		$memoize = $this->memoize;
+
+		$expected = array(1);
+		$this->assertEquals($expected, $memoize(1));
+		$this->assertEquals(1, $this->executed[\json_encode($expected)]);
+
+		$memoize->clean(1);
+
+		$this->assertEquals($expected, $memoize(1));
+		$this->assertEquals(2, $this->executed[\json_encode($expected)]);
+	}
+
+	public function testClean2Args() {
+		$memoize = $this->memoize;
+
+		$expected = array(1, 2);
+		$this->assertEquals($expected, $memoize(1, 2));
+		$this->assertEquals(1, $this->executed[\json_encode($expected)]);
+
+		$memoize->clean(2, 2);
+
+		$this->assertEquals($expected, $memoize(1, 2));
+		$this->assertEquals(1, $this->executed[\json_encode($expected)]);
+
+		$memoize->clean(1, 2);
+
+		$this->assertEquals($expected, $memoize(1, 2));
+		$this->assertEquals(2, $this->executed[\json_encode($expected)]);
+	}
+
+	public function testClear() {
+		$memoize = $this->memoize;
+
+		$this->assertEquals(array(1, 2), $memoize(1, 2));
+		$this->assertEquals(1, $this->executed[\json_encode(array(1, 2))]);
+
+		$this->assertEquals(array(2, 2), $memoize(2, 2));
+		$this->assertEquals(1, $this->executed[\json_encode(array(2, 2))]);
+
+		$this->assertEquals(array(1), $memoize(1));
+		$this->assertEquals(1, $this->executed[\json_encode(array(1))]);
+
+		$memoize->clear();
+
+		$this->assertEquals(array(1, 2), $memoize(1, 2));
+		$this->assertEquals(2, $this->executed[\json_encode(array(1, 2))]);
+
+		$this->assertEquals(array(2, 2), $memoize(2, 2));
+		$this->assertEquals(2, $this->executed[\json_encode(array(2, 2))]);
+
+		$this->assertEquals(array(1), $memoize(1));
+		$this->assertEquals(2, $this->executed[\json_encode(array(1))]);
+	}
+}
