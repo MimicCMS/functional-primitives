c1600cb6fcb919d75ca3fa131023801227e285fa
Jacob Santos <jacob@essential-elements.net>
2015-07-09 23:52:19 -0500

    spl_object_hash is dynamic, tests need to use regexp. First pass.

--- tests/HashArrayFuncTest.php
+++ tests/HashArrayFuncTest.php
@@ -13,38 +13,40 @@ use Mimic\Functional as F;
 class HashArrayFuncTest extends PHPUnit_Framework_TestCase {
 	public function dataProvider() {
 		$callback = function() { return true; };
+		$invokeTrue = 'Mimic\\\\Test\\\\Stub\\\\InvokeTrue';
+		$invokeFalse = 'Mimic\\\\Test\\\\Stub\\\\InvokeFalse';
 		return array(
 			array(
 				array(),
-				'[]'
+				'/\[\]/'
 			),
 			array(
 				array(1, 2, 3),
-				'[1,2,3]'
+				'/\[1,2,3\]/'
 			),
 			array(
 				array(new Stub\InvokeTrue, new Stub\InvokeFalse, 1, 2),
-				'["Mimic\\Test\\Stub\\InvokeTrue:000000005c48c5350000000052ff7dbb","Mimic\\Test\\Stub\\InvokeFalse:000000005c48c5320000000052ff7dbb",1,2]'
+				'/\["'.$invokeTrue.'\:[0-9a-zA-Z]+","'.$invokeFalse.'\:[0-9a-zA-Z]+",1,2\]/'
 			),
 			array(
 				array(1, 2, new Stub\InvokeTrue, new Stub\InvokeFalse),
-				'[1,2,"Mimic\\Test\\Stub\\InvokeTrue:000000005c48c5330000000052ff7dbb","Mimic\\Test\\Stub\\InvokeFalse:000000005c48c5300000000052ff7dbb"]'
+				'/\[1,2,"'.$invokeTrue.'\:[0-9a-zA-Z]+","'.$invokeFalse.'\:[0-9a-zA-Z]+"\]/'
 			),
 			array(
 				array($callback),
-				'["Closure:000000005c48c5340000000052facefb"]'
+				'/\["Closure\:[0-9a-zA-Z]+"\]/'
 			),
 			array(
 				array(1, $callback),
-				'[1,"Closure:000000005c48c5340000000052facefb"]'
+				'/\[1,"Closure\:[0-9a-zA-Z]+"\]/'
 			),
 			array(
 				array(0, 1, $callback),
-				'[0,1,"Closure:000000005c48c5340000000052facefb"]'
+				'/\[0,1,"Closure:[0-9a-zA-Z]+"\]/'
 			),
 			array(
 				array(0, 1, $callback, new Stub\InvokeTrue),
-				'[0,1,"Closure:000000005c48c5340000000052facefb","Mimic\\Test\\Stub\\InvokeTrue:000000005c48c5310000000052ff7dbb"]'
+				'/\[0,1,"Closure\:[0-9a-zA-Z]+","'.$invokeTrue.'\:[0-9a-zA-Z]+"\]/'
 			),
 		);
 	}
@@ -54,6 +56,6 @@ class HashArrayFuncTest extends PHPUnit_Framework_TestCase {
 	 * @covers ::Mimic\Functional\hash_array
 	 */
 	public function testResults($args, $expected) {
-		$this->assertEquals($expected, F\hash_array($args));
+		$this->assertRegExp($expected, F\hash_array($args));
 	}
 }
