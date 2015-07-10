57c0ae8ba40e90c8420d28284baea93a492bec25
Jacob Santos <jacob@essential-elements.net>
2015-07-10 16:35:04 -0500

    Fix tests.

--- tests/UniqueFuncTest.php
+++ tests/UniqueFuncTest.php
@@ -14,12 +14,12 @@ class UniqueFuncTest extends PHPUnit_Framework_TestCase {
 	public function dataProvider() {
 		return array(
 			array(
-				array(1, '1', 2, '2', 3, '3', 4, 5, 4.5, 6, '6'),
+				array(0 => 1, 2 => '1', 3 => 2, 5 => '2', 6 => 3, 7 => '3', 8 => 4, 9 => 5, 10 => 4.5, 11 => 6, 13 => '6'),
 				array(1, 1, '1', 2, 2, '2', 3, '3', 4, 5, 4.5, 6, 6, '6'),
 				true, null
 			),
 			array(
-				array(1, 2, 3, 4, 5, 4.5, 6),
+				array(0 => 1, 3 => 2, 6 => 3, 8 => 4, 9 => 5, 10 => 4.5, 11 => 6),
 				array(1, 1, '1', 2, 2, '2', 3, '3', 4, 5, 4.5, 6, 6, '6'),
 				false, null
 			),
