b41de5803ff1ac1c13172c88737466836dc23e74
Jacob Santos <jacob@essential-elements.net>
2015-07-10 14:41:57 -0500

    Fix test. Only uses strict comparison.

--- tests/OnlyFuncTest.php
+++ tests/OnlyFuncTest.php
@@ -27,7 +27,7 @@ class OnlyFuncTest extends PHPUnit_Framework_TestCase {
 			array($collection, array('thing1' => 1), array('thing1')),
 			array($collection, array(0 => 1), array(0)),
 			array($collection, array(0 => 1, 2 => 3), array(0, 2)),
-			array($collection, array(0 => 1), array('0')),
+			array($collection, array(), array('0')),
 			array($collection, array(), array(5)),
 			array($collection, array('thing2' => 2), array('thing2')),
 			array($collection, array('thing3' => 3), array('thing3')),
