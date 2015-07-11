7a3e333e1ef7b24575b6c747f8c10bc0272776d0
Jacob Santos <jacob@essential-elements.net>
2015-07-11 17:31:43 -0500

    * Complete partial functions, except any, which I'm not sure how is supposed to work. * Add inline documentation to compose and pipeline and technically complete pipeline since it just reverses compose. Since compose does not work, pipeline does not work either, even if it is completed.

--- src/composition.php
+++ src/composition.php
@@ -32,12 +32,29 @@ namespace Mimic\Functional;
 
 use Closure;
 
+/**
+ * Compose executes callbacks passing each result to the next callback.
+ *
+ * If you pass callbacks `f` and `g`, then the result will be g(f()).
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests
+ * @link \Mimic\Functional\pipeline
+ *   Similar function that executes in reverse order.
+ *
+ * @param callable ...$callbacks
+ * @return mixed
+ */
 function compose() {
 	/** @todo Incomplete */
 }
 
 /**
  * Mutates or accesses implementation based on name.
+ *
+ * @api
+ * @since 0.1.0
  *
  * @param string $name
  *   It is better to namespace and set the name to something that won't conflict
@@ -66,16 +83,120 @@ function partialAny() {
 	/** @todo Incomplete */
 }
 
-function partialLeft() {
-	/** @todo Incomplete */
+/**
+ * Apply partial arguments to callback from the left.
+ *
+ * The arguments called in the returned closure will be applied first and then
+ * the arguments passed in the `partialLeft()` will be applied.
+ *
+ *     $callback = partialLeft('array_sum', 1, 2);
+ *     $result = $callback(3, 4, 5);
+ *     // $result is array_sum(3, 4, 5, 1, 2);
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests
+ *
+ * @param callable $callback
+ * @param mixed ...$arguments
+ *   These will be applied last to the callback.
+ * @return Closure {
+ *     Call will execute with arguments combined.
+ *
+ *     @param mixed ...$arguments
+ *       These will be applied first to callback.
+ *     @return mixed
+ * }
+ */
+function partialLeft($callback) {
+	$outerArguments = array_slice(func_get_args(), 1);
+	return function() use ($callback, $outerArguments) {
+		$innerArguments = func_get_args();
+		return call_user_func_array($callback, array_merge($innerArguments, $outerArguments));
+	};
 }
 
-function partialRight() {
-	/** @todo Incomplete */
+/**
+ *
+ * @param string $methodName
+ * @param mixed $default
+ *   Returned when object and method name are not callable. Meaning object does
+ *   not have a method with that name or matching the execution. Care must be
+ *   taken that the correct instance or class method exists. PHP may decide that
+ *   it is acceptable to execute an object that is a string along with the
+ *   method that exists. If the method is in fact instance method, then
+ *   execution will result in an error. Likewise with the reverse.
+ * @param mixed ...$arguments
+ *   These will be applied last to the callback.
+ * @return Closure {
+ *     Call will execute on object with original method.
+ *
+ *     @param string|object $object
+ *     @param mixed ...$arguments
+ *       These will be applied first to callback.
+ *     @return mixed
+ * }
+ */
+function partialMethod($methodName, $default = null) {
+	$outerArguments = array_slice(func_get_args(), 2);
+	return function($object) use ($methodName, $default, $outerArguments) {
+		if ( ! is_callable(array($object, $methodName)) ) {
+			return $default;
+		}
+		$innerArguments = array_slice(func_get_args(), 1);
+		return call_user_func_array(array($object, $methodName), array_merge($innerArguments, $outerArguments));
+	};
 }
 
+/**
+ * Apply partial arguments to callback from the right.
+ *
+ * The arguments passed to this function will be first and the arguments passed
+ * in the return closure will be called last.
+ *
+ *     $callback = partialLeft('array_sum', 1, 2);
+ *     $result = $callback(3, 4, 5);
+ *     // $result is array_sum(1, 2, 3, 4, 5);
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests
+ *
+ * @param callable $callback
+ * @param mixed ...$arguments
+ *   These will be applied first to the callback.
+ * @return Closure {
+ *     Call will execute with arguments combined.
+ *
+ *     @param mixed ...$arguments
+ *       These will be applied last to callback.
+ *     @return mixed
+ * }
+ */
+function partialRight($callback) {
+	$innerArguments = func_get_args();
+	return function() use ($callback, $innerArguments) {
+		$outerArguments = func_get_args();
+		return call_user_func_array($callback, array_merge($innerArguments, $outerArguments));
+	};
+}
+
+/**
+ * Pipeline executes callbacks in the order they are in the arguments.
+ *
+ * If you pass callbacks `f` and `g`, then the result will be f(g()).
+ *
+ * @api
+ * @since 0.1.0
+ * @todo Needs tests
+ * @link \Mimic\Functional\compose
+ *   Similar function that executes in reverse order.
+ *
+ * @param callable ...$callbacks
+ * @return mixed
+ */
 function pipeline() {
-	/** @todo Incomplete */
+	return compose(array_reverse(func_get_args()));
 }
 
 /**
@@ -83,7 +204,6 @@ function pipeline() {
  *
  * @api
  * @since 0.1.0
- * @todo Needs tests.
  *
  * @param callback $callback
  *   Every callback type is supported. Arguments will be passed to callback, if any exist.
