6fada0032e25f2396d8d0e317d7b427678f597fd
Jacob Santos <jacob@essential-elements.net>
2015-07-12 02:09:14 -0500

    Add sort function test suite.

---
+++ tests/SortFuncTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mimic\Test;
+
+use PHPUnit_Framework_TestCase;
+use Mimic\Functional as F;
+
+/**
+ * Unit Test for sort Mimic library function.
+ *
+ * @since 0.1.0
+ */
+class SortFuncTest extends PHPUnit_Framework_TestCase {
+	public function dataProvider() {
+		return array(
+			array(false, F\sortable(), array(4, 2, 1, 3), array(1, 2, 3, 4)),
+			array(true, F\sortable(), array(4, 2, 1, 3), array(2 => 1, 1 => 2, 3 => 3, 0 => 4)),
+			array(false, 'natcasesort',
+				array('2', '1', 'world', 'hello'),
+				array('1', '2', 'hello', 'world')
+			),
+			array(true, 'natcasesort',
+				array('2', '1', 'world', 'hello'),
+				array(2 => '1', 0 => '2', 4 => 'hello', 3 => 'world')
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @covers ::Mimic\Functional\sort
+	 */
+	public function testResults($preserveKeys, $callback, $collection, $expected) {
+		$this->assertEquals($expected, F\sort($collection, $callback, $preserveKeys));
+	}
+}
