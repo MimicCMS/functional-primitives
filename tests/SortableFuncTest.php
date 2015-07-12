7ad56a3471fdd956e8198ce24140e94ce302bd72
Jacob Santos <jacob@essential-elements.net>
2015-07-12 03:16:38 -0500

    Switch to data provider

--- tests/SortableFuncTest.php
+++ tests/SortableFuncTest.php
@@ -11,35 +11,41 @@ use Mimic\Functional as F;
  * @since 0.1.0
  */
 class SortableFuncTest extends PHPUnit_Framework_TestCase {
-	/**
-	 * @covers ::Mimic\Functional\sortable
-	 */
-	public function testNormal() {
-		$callback = F\sortable();
-		$this->assertEquals(0, $callback(0, 0));
-		$this->assertEquals(0, $callback(1, 1));
-		$this->assertEquals(0, $callback(1, '1'));
-		$this->assertEquals(-1, $callback(1, 2));
-		$this->assertEquals(-1, $callback(1, '2'));
-		$this->assertEquals(-1, $callback('1', '2'));
-		$this->assertEquals(1, $callback(2, 1));
-		$this->assertEquals(1, $callback('2', 1));
-		$this->assertEquals(0, $callback('something', 'something'));
+	public function dataProvider() {
+		return array(
+			array(0, 0, 0, false),
+			array(0, 1, '1', false),
+			array(-1, 1, 2, false),
+			array(-1, 1, '2', false),
+			array(-1, '1', '2', false),
+			array(1, 2, 1, false),
+			array(0, 0, 0, true),
+			array(0, 1, '1', true),
+			array(1, 1, 2, true),
+			array(1, 1, '2', true),
+			array(1, '1', '2', true),
+			array(-1, 2, 1, true),
+			array(0, 'something', 'something', false),
+			array(-1, 'hello', 'world', false),
+			array(1, 'world', 'hello', false),
+			array(0, 'something', 'something', true),
+			array(1, 'hello', 'world', true),
+			array(-1, 'world', 'hello', true),
+			array(0, array(), array(), false),
+			array(0, array(), array(), true),
+			array(-1, 1, array(), false),
+			array(1, 1, array(), true),
+			array(1, array(), 1, false),
+			array(-1, array(), 1, true),
+		);
 	}
 
 	/**
+	 * @dataProvider dataProvider
 	 * @covers ::Mimic\Functional\sortable
 	 */
-	public function testReversed() {
-		$callback = F\sortable(true);
-		$this->assertEquals(0, $callback(0, 0));
-		$this->assertEquals(0, $callback(1, 1));
-		$this->assertEquals(1, $callback(1, '1'));
-		$this->assertEquals(1, $callback(1, 2));
-		$this->assertEquals(1, $callback(1, '2'));
-		$this->assertEquals(1, $callback('1', '2'));
-		$this->assertEquals(-1, $callback(2, 1));
-		$this->assertEquals(-1, $callback('2', 1));
-		$this->assertEquals(0, $callback('something', 'something'));
+	public function testIntegers($expected, $left, $right, $reversed) {
+		$callback = F\sortable($reversed);
+		$this->assertEquals($expected, $callback($left, $right));
 	}
 }
