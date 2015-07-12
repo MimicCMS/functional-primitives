80e3af07095e1f98b893acc9e84403eca75b5c91
Jacob Santos <jacob@essential-elements.net>
2015-07-12 05:40:57 -0500

    Rename class to match renamed function.

--- tests/FlattenRecursiveFuncTest.php
+++ tests/FlattenRecursiveFuncTest.php
@@ -8,27 +8,32 @@ use Mimic\Functional as F;
 use ArrayIterator;
 
 /**
- * Unit Test for flatten Mimic library function.
+ * Unit Test for flattenRecursive Mimic library function.
  *
  * @since 0.1.0
  *
  * @todo Need to use QuickTest library.
  */
-class FlattenFuncTest extends PHPUnit_Framework_TestCase {
+class FlattenRecursiveFuncTest extends PHPUnit_Framework_TestCase {
 	public function dataProvider() {
 		return array(
 			array(array(1, 2, 3, 1, 2, 3), array(1, 2, 3, array(1, 2, 3))),
+			array(array(1, 2, 3, 4, 5, 6, 7, 8, 9), array(1, 2, 3, array(4, 5, 6), array(array(7,8, 9)))),
 			array(array('hello', 'world', 'something', 'else'), array('hello', 'world', array('something', 'else'))),
 			array(array(1, 2, 3, 0, 'else'), array(0 => 1, 4 => 2, 5 => 3, array(1 => 0, 'something' => 'else'))),
 			array(array(1, 2, 3, 4, 5, 6), new ArrayIterator(array(1, 2, 3, array(4, 5, 6)))),
+			array(
+				array(1, 2, 3, 4, 5, 6, 7, 8, 9),
+				new ArrayIterator(array(1, 2, 3, array(4, 5, 6), array(array(7,8, 9))))
+			),
 		);
 	}
 
 	/**
 	 * @dataProvider dataProvider
-	 * @covers ::Mimic\Functional\flatten
+	 * @covers ::Mimic\Functional\flattenRecursive
 	 */
 	public function testResults($expected, $collection) {
-		$this->assertEquals($expected, F\flatten($collection));
+		$this->assertEquals($expected, F\flattenRecursive($collection));
 	}
 }
