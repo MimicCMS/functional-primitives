4dfcabf74fd17e170e37487c9ac97bbf88c08dc5
Jacob Santos <jacob@essential-elements.net>
2015-07-13 12:14:41 -0500

    Complete forget first test suite. Should cover all code paths.

--- tests/ForgetFirstFuncTest.php
+++ tests/ForgetFirstFuncTest.php
@@ -23,12 +23,53 @@ class ForgetFirstFuncTest extends PHPUnit_Framework_TestCase {
 		);
 	}
 
+	public function dataProvider_removeSecond() {
+		return array(
+			array(array(1, 2, 3), array(1, 2, 3), array(1, 3)),
+			array(new ArrayIterator(array(1, 2, 3)), array(1, 2, 3), array(1, 3)),
+			array(array('something', 'else', 'hello', 'world'), array('something', 'else', 'hello', 'world'), array('something', 'hello', 'world')),
+			array(array('else'), array('else'), array('else')),
+			array(array(), array(), array()),
+		);
+	}
+
 	/**
 	 * @dataProvider dataProvider
 	 * @covers ::Mimic\Functional\forgetFirst
 	 */
 	public function testResults($collection, $check, $expected) {
 		$this->assertEquals($expected, F\forgetFirst($collection));
+		$this->assertSame($check, $collection, 'collection has changed');
+	}
+
+	/**
+	 * @dataProvider dataProvider
+	 * @covers ::Mimic\Functional\forgetFirst
+	 */
+	public function testResultWithCallbacks_keepAll($collection) {
+		$self = $this;
+		$callback = function($element, $index, $array) use ($self, $collection) {
+			$self->assertSame($collection, $array, 'callback collection is different from given');
+			$self->assertEquals($collection[$index], $element, 'element does not match element with index at given collection');
+			return false;
+		};
+		$this->assertEquals($collection, F\forgetFirst($collection, $callback));
+	}
+
+	/**
+	 * @dataProvider dataProvider_removeSecond
+	 * @covers ::Mimic\Functional\forgetFirst
+	 */
+	public function testResultWithCallbacks_removeSecond($collection, $check, $expected) {
+		$self = $this;
+		$at = 0;
+		$callback = function($element, $index, $array) use (&$at, $self, $collection) {
+			$self->assertSame($collection, $array, 'callback collection is different from given');
+			$self->assertEquals($collection[$index], $element, 'element does not match element with index at given collection');
+			$at += 1;
+			return $at === 2;
+		};
+		$this->assertEquals($expected, F\forgetFirst($collection, $callback));
 		$this->assertSame($check, $collection, 'collection has changed');
 	}
 }
