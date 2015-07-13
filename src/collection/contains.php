fd34cb17f6eb409d44164a3a2fa5847f851cbdf3
Jacob Santos <jacob@essential-elements.net>
2015-07-13 13:50:59 -0500

    * Remove todo inline documentation. * Moved functions from comparison to this file.

--- src/collection/contains.php
+++ src/collection/contains.php
@@ -91,6 +91,34 @@ function falsy($collection) {
 }
 
 /**
+ * First index in collection matching value, using value as callback if
+ * available.
+ *
+ * @api
+ * @since 0.1.0
+ *
+ * @param Traversable|array $collection
+ * @param MapCollectionCallback|callable|mixed $value
+ *   A mixed value allows checking for the given value against element returning
+ *   index in collection.
+ * @param boolean $strict
+ *   Optional. Defaults to true. Only used when value is mixed or not callable.
+ * @return boolean|string|numeric
+ *   False on failure.
+ */
+function firstIndexOf($collection, $value, $strict = true) {
+	if ( is_callable($value) ) {
+		foreach ( $collection as $index => $element ) {
+			if ( $value($element, $index, $collection) ) {
+				return $index;
+			}
+		}
+		return false;
+	}
+	return firstIndexOf($collection, compare($value, $strict));
+}
+
+/**
  * Whether collection has index.
  *
  * @api
@@ -104,6 +132,51 @@ function has($collection, $key) {
 	return short($collection, true, false, function($_, $index) use ($key) {
 		return $index === $key;
 	});
+}
+
+/**
+ * List of all indexes that match elements in a collection.
+ *
+ * @api
+ * @since 0.1.0
+ *
+ * @param Traversable|array $collection
+ * @param MapCollectionCallback|callable|mixed $value
+ *   A mixed value allows checking for the given value against element returning
+ *   index in collection.
+ * @param boolean $strict
+ *   Optional. Defaults to true. Only used when value is mixed or not callable.
+ * @return boolean|string|numeric
+ *   False on failure.
+ */
+function indexesOf($collection, $value, $strict = true) {
+	if ( is_callable($value) ) {
+		return array_keys(filter($collection, $value));
+	}
+	return indexesOf($collection, compare($value, $strict));
+}
+
+/**
+ * Last index in collection matching value, using value as callback if
+ * available.
+ *
+ * @api
+ * @since 0.1.0
+ *
+ * @param Traversable|array $collection
+ * @param MapCollectionCallback|callable|mixed $value
+ *   A mixed value allows checking for the given value against element returning
+ *   index in collection.
+ * @param boolean $strict
+ *   Optional. Defaults to true. Only used when value is mixed or not callable.
+ * @return boolean|string|numeric
+ *   False on failure.
+ */
+function lastIndexOf($collection, $value, $strict = true) {
+	if ( is_callable($value) ) {
+		return firstIndexOf(array_reverse($collection, true), passOriginalCollection($collection, $value));
+	}
+	return firstIndexOf(array_reverse($collection, true), $value, $strict);
 }
 
 /**
@@ -157,7 +230,6 @@ function short($collection, $passed, $default, $callback) {
  *
  * @api
  * @since 0.1.0
- * @todo Needs tests.
  *
  * @param Traversable|array $collection
  * @param MapCollectionCallback|callable $callback
